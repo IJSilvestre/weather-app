fe5f234231867e2f835413489be7d5529e5a181c
Irley Jose Silvestre <133180677+IJSilvestre@users.noreply.github.com>
2023-08-23 08:59:12 -0300

    add awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { WeatherHomeComponent } from './modules/weather/pages/weather-home/weather-home.component';
 import { WeatherAboutComponent } from './modules/weather/pages/weather-about/weather-about.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, WeatherHomeComponent, WeatherAboutComponent],
@@ -15,6 +16,7 @@ import { WeatherAboutComponent } from './modules/weather/pages/weather-about/wea
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
