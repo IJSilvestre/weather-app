ae034b7b34c10dcf5555e17aecb601dfc82df5d3
Irley Jose Silvestre <133180677+IJSilvestre@users.noreply.github.com>
2023-08-23 09:03:03 -0300

    add error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WeatherHomeComponent } from './modules/weather/pages/weather-home/weather-home.component';
 import { WeatherAboutComponent } from './modules/weather/pages/weather-about/weather-about.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'weather' },
   { path: 'weather', component: WeatherHomeComponent },
   { path: 'weather-about', component: WeatherAboutComponent },
+  { path: '**', component: ErrorPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { WeatherHomeComponent } from './modules/weather/pages/weather-home/weather-home.component';
 import { WeatherAboutComponent } from './modules/weather/pages/weather-about/weather-about.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
-  declarations: [AppComponent, WeatherHomeComponent, WeatherAboutComponent],
+  declarations: [AppComponent, WeatherHomeComponent, WeatherAboutComponent, ErrorPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
