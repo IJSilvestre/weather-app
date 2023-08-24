ed0f25c7d1ee6e18813446d8c04120d4df96f329
IrleySilvestre <irleysilvestre.mba@gmail.com>
2023-08-23 21:18:58 -0300

    add service

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  private apiKey: string = '746b33081bcb1cec74aeb3d8d9612de8';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+  getWeatherData(lat: string, lon: string): Observable<any> {
+    return this.httpClient.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${this.apiKey}`
+    );
+  }
 }
