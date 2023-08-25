669c03206481b9654102c8843d12c18536325a9c
Irley Jose Silvestre <133180677+IJSilvestre@users.noreply.github.com>
2023-08-25 20:03:45 -0300

    add service

--- src/app/modules/weather/pages/weather-home/weather-home.component.ts
+++ src/app/modules/weather/pages/weather-home/weather-home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: ['./weather-home.component.scss']
+  styleUrls: ['./weather-home.component.scss'],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+  getWeaather(lat: string, lon: string): void {
+    this.weatherService.getWeatherData(lat, lon).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (error) => console.log(error),
+    });
+  }
 
+  ngOnInit(): void {
+    this.getWeaather('-16.63401602789569', '-49.209006296451584');
+
+  }
 }
--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -10,8 +10,10 @@ export class WeatherService {
 
   constructor(private httpClient: HttpClient) {}
   getWeatherData(lat: string, lon: string): Observable<any> {
+    console.log(lat, lon)
     return this.httpClient.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${this.apiKey}`
+      // `https://api.openweathermap.org/data/3.0/onecall?lat=-16.63401602789569&lon=-49.209006296451584&exclude=hourly,daily&appid=${this.apiKey}`
+      `https://viacep.com.br/ws/74884655/json/`
     );
   }
 }
